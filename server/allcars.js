9a5777afbc8352fddb4b089ce1d649f156b3b81f
NabilMouami <86678449+NabilMouami@users.noreply.github.com>
2022-02-05 22:09:47 +0100

    Add files via upload

---
+++ server/allcars.js
@@ -0,0 +1,46 @@
+const allcars =[
+    {
+        id:0,
+        image: "./images/Car01.jpeg",
+        name: 'Citroin 2020',
+        prix: 'US$ 21 per day'
+     },
+    {
+       id:1,
+       image: "./images/vehicle-1.jpg",
+       name: 'Citroin 2020',
+       prix: 'US$ 21 per day'
+    },
+    {
+        id:2,
+        image: './images/vehicle-2.jpg',
+        name: 'Renau 2019',
+        prix: 'US$ 15 per day'
+     },
+     {
+        id:3,
+        image: './images/vehicle-3.jpg',
+        name: 'Honda Bri 2020',
+        prix: 'US$ 25 per day'
+     },
+     {
+        id:4,
+        image: './images/vehicle-4.jpg',
+        name: 'Tesla model s3 2017',
+        prix: 'US$ 40 per day'
+     },
+     {
+        id:5,
+        image: "./images/vehicle-5.jpg",
+        name: 'Citroin 2020',
+        prix: 'US$ 21 per day'
+     },
+     {
+        id:6,
+        image: "./images/vehicle-6.jpg",
+        name: 'Citroin 2020',
+        prix: 'US$ 21 per day'
+     },
+
+];
+module.exports = allcars;
---
+++ server/cars.js
@@ -0,0 +1,28 @@
+const cars =[
+    {
+       id:1,
+       image: "./images/vehicle-1.jpg",
+       name: 'Citroin 2020',
+       prix: 'US$ 21 per day'
+    },
+    {
+        id:2,
+        image: './images/vehicle-2.jpg',
+        name: 'Renau 2019',
+        prix: 'US$ 15 per day'
+     },
+     {
+        id:3,
+        image: './images/vehicle-3.jpg',
+        name: 'Honda Bri 2020',
+        prix: 'US$ 25 per day'
+     },
+     {
+        id:4,
+        image: './images/vehicle-4.jpg',
+        name: 'Tesla model s3 2017',
+        prix: 'US$ 40 per day'
+     },
+
+];
+module.exports = cars;
